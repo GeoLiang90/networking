5b7068e3946c0a1f818fab4cb0498e5db51c5fa6
George <gliang90@stuy.edu>
2018-12-19 01:52:27 -0500

    finish

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,6 +1,5 @@
 #include "pipe_networking.h"
 
-
 /*=========================
   server_handshake
   args: int * to_client
@@ -11,6 +10,41 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  printf("Creating upstream pipe \n");
+  int pipe = mkfifo("WKP",0644);
+
+  if (pipe != 0){
+  perror("Error creating upstream pipe \n");
+  return -1;
+  }
+
+  printf("Created upstream pipe\n");
+
+  int up_pipe = open("WKP",O_RDONLY);
+  char s[HANDSHAKE_BUFFER_SIZE];
+
+  read(up_pipe,s,HANDSHAKE_BUFFER_SIZE);
+
+
+  if (strcmp(s,ACK)== 0){
+    printf("Client message successfully received\n");
+    *to_client = open("DP",O_WRONLY);
+
+    if (*to_client < 0){
+      perror("Error opening downpipe from client to write\n");
+      return -1;
+    }
+
+    printf("Writing to client\n");
+    write(*to_client,ACK,sizeof(ACK));
+    printf("Wrote message to client\n");
+    return up_pipe;
+  }
+  else{
+    printf("Client message verification error\n");
+    printf("%s\n",s);
+    return -1;
+  }
   return 0;
 }
 
@@ -25,5 +59,39 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+  printf("Opening comms to WKP \n");
+  int comm = open("WKP", O_WRONLY);
+
+  if (comm < 0){
+    perror("Error opening up to WKP \n");
+  }
+
+  printf("Now writing ACK to the WKP \n");
+
+  write(comm, ACK, sizeof(ACK));
+
+  *to_server = comm;
+
+  printf("Now opening downstream from server to read returns \n");
+  int l = mkfifo("DP",0644);
+  if (l != 0){
+    perror("Error with downpipe");
+  }
+
+  int dcomm = open("DP", O_RDONLY);
+
+  if (dcomm < 0){
+    perror("Error opening up to downpipe \n");
+  }
+
+  char s[HANDSHAKE_BUFFER_SIZE];
+
+  read(dcomm,s,HANDSHAKE_BUFFER_SIZE);
+  if (strcmp(s,ACK) == 0){
+    "Successfully received info from server \n";
+  }
+  printf("Replying to server and finishing\n");
+  char * g = "Got message";
+  write(comm,g, sizeof(g));
+  return dcomm;
 }
